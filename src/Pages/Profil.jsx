43fd2f2710fcbad2ff91eb81c1a1757e04b9623f
Eric <eric.antonie.noel92@gmail.com>
2024-04-22 10:42:56 +0200

    page profil

--- src/Pages/Profil.jsx
+++ src/Pages/Profil.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import './Profil.css';
+import { Link } from 'react-router-dom';
 export default class Profil extends Component {
   render() {
     return (
       <div>
-        
+         <Link to="/HomePage">
+        <button className="HomePage-button">HomePage</button>
+    </Link>
       </div>
     )
   }
